8ce736ccd53ed14690a2b1cf66080f9153f286a7
AARYAN JINDAL <aaryanjindal15@gmail.com>
2024-04-25 22:14:52 +0530

    ab

--- s1.c
+++ s1.c
@@ -1,4 +1,37 @@
-    #include<stdio.h>
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    char operator;
+    printf("ENTER THE FIRST NUMBER ");
+    scanf("%d",&a);
+    printf("ENTER THE SECOND NUMBER ");
+    scanf("%d",&b);
+    printf("SELECT THE OPERATOR + - * / ");
+    scanf(" %c",&operator);
+    if(operator == '+'){
+        c= a+b;
+        printf("%d",(c));
+
+    }
+    else if(operator == '-'){
+        c = a-b;
+        printf("%d",(c));
+
+    }
+    else if(operator == '*'){
+        c = a*b;
+        printf("%d",(c));
+
+    }
+    else{
+        c = a/b;
+        printf("%d",(c));
+    }
+    return 0;
+}
+
+    
+//     #include<stdio.h>
 
 // int main() {
 //     int a, b, c;
